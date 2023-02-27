47a60bca7b5ed97cca33e539cd38811aa5e917f4
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-02-27 16:10:42 +0800

    Add other types of oscilators

--- src/synth.rs
+++ src/synth.rs
@@ -3,22 +3,41 @@ use rodio::source::Source;
 
 const SAMPLE_RATE: u32 = 48000; // The sample rate of the audio in Hz.
 
-// An infinite source that produces a sine.
-// Always has a rate of 48kHz and one channel.
+// The wave type of the synth
+#[derive(Clone, Debug)]
+enum WaveType {
+	Sine,
+	Square,
+	Sawtooth,
+	Triangle,
+}
+
+
 #[derive(Clone, Debug)]
 pub struct Synth {
     freq: f32,
     num_sample: usize,
+	wave_type: WaveType,
 }
 
 impl Synth {
-    // The frequency of the sine.
-    pub fn new(freq: f32) -> Synth {
+	#[allow(dead_code)]
+    pub fn sine_wave(freq: f32) -> Synth {
         Synth {
             freq: freq,
             num_sample: 0,
+			wave_type: WaveType::Sine,
         }
     }
+
+	#[allow(dead_code)]
+	pub fn square_wave(freq: f32) -> Synth {
+		Synth {
+			freq: freq,
+			num_sample: 0,
+			wave_type: WaveType::Square,
+		}
+	}
 }
 
 impl Iterator for Synth {
@@ -28,7 +47,14 @@ impl Iterator for Synth {
         self.num_sample = self.num_sample.wrapping_add(1);
 
         let value = 2.0 * PI * self.freq * self.num_sample as f32 / SAMPLE_RATE as f32;
-        Some(value.sin().signum())
+
+		match self.wave_type {
+			WaveType::Sine => Some(value.sin()), // Sine wave
+			WaveType::Square => Some(value.sin().signum()), // Signing the sine wave locks it to 1 or -1, making it a square wave.
+			WaveType::Sawtooth => Some(value.sin().atan()), // The arctangent of the sine wave makes it a sawtooth wave.
+			WaveType::Triangle => Some(value.sin().asin()), // The arcsine of the sine wave makes it a triangle wave.
+			_ => None,
+		}
     }
 }
 
